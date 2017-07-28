6b214aa8407e36ff5636a0bc585e4a004f24c61b
Mikael Håkansson <wmmihaa@hotmail.com>
2017-07-28 13:49:44 +0200

    '''

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 #!/usr/bin/env node
-console.log("requiring package.json");
-console.log("base folder = " + __dirname);
-var pjson = require('./package.json');
+console.log("start");
+var npm = require('npm');
+var thermometer;
+
+addNpmPackage("iot-simlators", function (err) {
+    var Thermometer = require(process.env.__npmPath+"iot-simlators").Thermometer;
+    thermometer = new Thermometer(null, 39);
+});
+
+setTimeout(function () {
+    thermometer.start(1000, function (r) {
+        console.log(r.temperature);
+    })
+}, 5000);
+function addNpmPackage(npmPackage, callback) {
+    var ret;
+    var me = this;
+
+    npm.load({ loaded: true }, function (err) {
+        process.env.__npmPath = npm.dir + "/";
+
+        // npm.on("log", function (message) {
+        //     ret = null;
+        // });
+        // npm.on("error", function (error) {
+        //     ret = null;
+        // });
+        npm.commands.install([npmPackage], function (er, data) {
+            callback(er);
+        });
+    });
+};
