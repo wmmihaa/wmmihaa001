f8cd97cb055eab3f7255d5059841370d72a63779
Mikael Håkansson <wmmihaa@hotmail.com>
2017-07-28 21:05:52 +0200

    ''

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
+
+
+if (!process.env.__npmPath) {
+    process.env.__npmPath = __dirname + "/node_modules"
+}
+
 console.log("start");
 var util = require('./lib/Util.js');
 var thermometer;
 
 util.showPaths("Main");
 
-util.addNpmPackage("iot-simlators@1.0.6", function (err) {
+util.addNpmPackage("iot-simlators@1.0.7", function (err) {
     console.log("Package installed");
 
-    var Thermometer = require(process.env.__npmPath + "iot-simlators").Thermometer;
+    var Thermometer = util.require("iot-simlators").Thermometer;
     thermometer = new Thermometer(null, 39);
     // thermometer.start(1000, function (r) {
     //     console.log(r.temperature);
