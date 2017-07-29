0ac0af0163c573ba462ff83eea671def35efa417
Mikael Håkansson <wmmihaa@hotmail.com>
2017-07-29 23:41:19 +0200

    ssss

--- index.js
+++ index.js
@@ -3,34 +3,34 @@
 if (!process.env.__npmPath) {
     process.env.__npmPath = __dirname + "/node_modules"
 }
-process.env.NODE_PATH = __dirname+ ";" + __dirname + "/node_modules";
+process.env.NODE_PATH = __dirname + ";" + __dirname + "/node_modules";
 require('module').Module._initPaths();
 
-// var path = require("path");
-// var Module = require('module').Module;
+var path = require("path");
+var Module = require('module').Module;
 
-// console.log('Before');
-// for (var i = 0; i < require.main.paths.length; i++) {
-//     console.log(require.main.paths[i]);
-// }
-// console.log('');
+console.log('Before');
+for (var i = 0; i < require.main.paths.length; i++) {
+    console.log(require.main.paths[i]);
+}
+console.log('');
 
-// packagePath = path.resolve(process.env.APPDATA ? process.env.APPDATA : ".", "node_modules");
-// require('app-module-path').addPath(packagePath);
-// require('app-module-path').addPath(__dirname);
-// Module.globalPaths.push(packagePath);
-// console.log('');
+packagePath = path.resolve(process.env.APPDATA ? process.env.APPDATA : ".", "node_modules");
+require('app-module-path').addPath(packagePath);
+require('app-module-path').addPath(__dirname);
+Module.globalPaths.push(packagePath);
+console.log('');
 
-// for (var i = 0; i < require.main.paths.length; i++) {
+for (var i = 0; i < require.main.paths.length; i++) {
 
-//     if(Module.globalPaths.indexOf(require.main.paths[i]) === -1)
-//         Module.globalPaths.push(require.main.paths[i]);
-// }
+    if (Module.globalPaths.indexOf(require.main.paths[i]) === -1)
+        Module.globalPaths.push(require.main.paths[i]);
+}
 
-// console.log('After');
-// for (var i = 0; i < require.main.paths.length; i++) {
-//     console.log(require.main.paths[i]);
-// }
+console.log('After');
+for (var i = 0; i < require.main.paths.length; i++) {
+    console.log(require.main.paths[i]);
+}
 require('colors');
 console.log("start".bgYellow.black);
 var util = require('./lib/Util.js');
